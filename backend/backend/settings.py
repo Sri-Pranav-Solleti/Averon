367bcabdff7477f8bf1a7b3a975ef3be43bbdcc9
Sri-Pranav-Solleti <solletipranav@gmail.com>
2026-05-19 13:46:20 +0530

    fixed allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "averon-production-534f.up.railway.app",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
